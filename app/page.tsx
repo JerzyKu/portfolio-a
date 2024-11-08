5a79576d189211cbf9a0607d458137f13881b47d
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-11-08 13:41:46 +0100

    pre mdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import Intro from './intro'
 
 type Props = {}
 
 function Home({}: Props) {
   return (
-    <section className='py-24'>
-      <div className="container">
-          <h1 className="text-3xl font-bold">Portfolio</h1>
+    <>
+    <section className='py-24  '>
+      <div className="container max-w-3xl">
+          {/* <h1 className="text-3xl font-bold">Portfolio</h1> */}
+          <Intro />
       </div>
     </section>
+    </>
   )
 }
 
