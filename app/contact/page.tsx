017ab958d521106d8c685183603fa443dbbd49e4
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-11-09 11:24:47 +0100

    save

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1 className='title mb-12'>Contact</h1>
+
+      </div>
+    </section>
+  )
+}
